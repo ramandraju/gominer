0d02b31b712bea88f76ef83d3ed7280f3c0b11f1
sai <ram999.mail@gmail.com>
2017-08-18 17:53:37 +0530

    Update siastratum.go

--- algorithms/sia/siastratum.go
+++ algorithms/sia/siastratum.go
@@ -10,8 +10,8 @@ import (
 	"time"
 
 	"github.com/dchest/blake2b"
-	"github.com/robvanmieghem/gominer/clients"
-	"github.com/robvanmieghem/gominer/clients/stratum"
+	"github.com/ramandraju/gominer/clients"
+	"github.com/ramandraju/gominer/clients/stratum"
 )
 
 const (
