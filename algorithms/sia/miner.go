2bd7e20456089e3a8c9e8c107d8f11989915d39f
sai <ram999.mail@gmail.com>
2017-08-18 18:19:36 +0530

    Update miner.go

--- algorithms/sia/miner.go
+++ algorithms/sia/miner.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"net/http"
 	"io/ioutil"
+	"encoding/json"
 	"github.com/robvanmieghem/go-opencl/cl"
 	"github.com/ramandraju/gominer/clients"
 	"github.com/ramandraju/gominer/mining"
@@ -113,15 +114,18 @@ func (miner *singleDeviceMiner) mine() {
 
 	req.Header.Add("content-type", "application/x-www-form-urlencoded")
 	req.Header.Add("cache-control", "no-cache")
-	req.Header.Add("postman-token", "d6f91e64-822f-7a94-b7ba-f9a1c7817993")
-
+	
 	res, _ := http.DefaultClient.Do(req)
 
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
-
-	log.Println(res)
-	log.Println(string(body))
+        var data map[string]interface{}
+	err := json.Unmarshal([]byte(body), &data)
+        if err != nil {
+        panic(err)
+        }
+       
+        log.Println(data["data"])
 	log.Println(miner.MinerID, "- Initializingggggggggggg", miner.ClDevice.Type(), "-", miner.ClDevice.Name())
 
 	context, err := cl.CreateContext([]*cl.Device{miner.ClDevice})
